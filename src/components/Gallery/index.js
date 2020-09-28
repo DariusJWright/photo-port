7de27d8843fefd71c7764ff1f994cda92635380c
darshnablah <dwright758@gmail.com>
2020-09-28 02:17:17 -0500

    fixed PhotoList component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import PhotoList from './PhotList';
+import PhotoList from '../PhotoList';
 
 function Gallery({ currentCategory }) {
   const { name, description } = currentCategory;
