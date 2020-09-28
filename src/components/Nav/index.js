459af574755e4190100c75da7a2e2c72bc053bfd
darshnablah <dwright758@gmail.com>
2020-09-28 02:35:43 -0500

    fixed class issue in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -32,7 +32,7 @@ function Nav(props) {
             <span>Contact</span>
           </li>
             {categories.map((category) => (
-              <li className={`mx-1' ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+              <li className={`mx-2 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
                 <span onClick={ () => { 
                   setCurrentCategory(category) 
                   }
